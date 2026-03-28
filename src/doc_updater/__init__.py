8c18a2b236c9e2778819b69397bb411ba99214c3
Paul Tiplady <paultiplady@gmail.com>
2026-03-28 13:37:45 -0700

    Bump version to 0.2.0

--- src/doc_updater/__init__.py
+++ src/doc_updater/__init__.py
@@ -4,5 +4,5 @@ from doc_updater.document import Document
 from doc_updater.frontmatter import ReviewConfig
 from doc_updater.reviewer import DocumentReviewer
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = ["Document", "ReviewConfig", "DocumentReviewer", "__version__"]
